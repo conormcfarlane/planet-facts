04b83548cb43b7135d769da18f484b740a162aa4
conormcfarlane <conormcf98@gmail.com>
2025-07-03 19:06:18 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      {/* When defining routes wrap content in Routes tag and use Route path tag for each....home page will be path "/"  */}
+    </>
+  );
+}
+
+export default App;
