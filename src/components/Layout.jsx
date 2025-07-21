dce972d2f54a0e89178e7e17525aa9ef69704e84
conormcfarlane <conormcf98@gmail.com>
2025-07-21 18:43:23 +1000

    Dynamically rendered Mobile Menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import MobileMenu from "./MobileMenu";
+import { useMenu } from "../context/MenuContext";
 export default function Layout() {
+  const {isMenuOpen} = useMenu();
   return (
     <div>
       <Header />
-      <MobileMenu/>
+      {isMenuOpen && <MobileMenu/>}
+      
     </div>
   );
 }
