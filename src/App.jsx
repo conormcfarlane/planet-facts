181cb11a2d6ac5da31affecebb72c07aebbb14d8
conormcfarlane <conormcf98@gmail.com>
2025-07-30 19:45:24 +1000

    Add missing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import PlanetPage from "./components/PlanetPage";
 import backgroundStars from "./Assets/Images/background-stars.svg";
 import { TabContextProvider } from "./Context/TabContext";
